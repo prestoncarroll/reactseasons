3b57231422cdb3a33eae5bdd96f2e03eb764e6f1
Preston Carroll <plutusinc1@yahoo.com>
2020-06-23 17:49:50 -0500

    loading screen set, loading screen component created

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,10 @@
+import './SeasonDisplay.css'
 import React from 'react';
 
 
 
 
-const seasonConfig ={
+const seasonConfig = {
     summer: {
         text: 'lets hit the beach',
         iconName: 'sun'
@@ -30,18 +31,18 @@ const SeasonDisplay = props => {
 
     const season = getSeason(props.lat, new Date().getMonth());
 
-    const {text, iconName } = seasonConfig[season]// {text, iconName}
+    const { text, iconName } = seasonConfig[season]// {text, iconName}
 
 
 
     console.log({ text })
     return (
-        <div>
-            <i className={`${iconName} icon`} />
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
             <h1>
                 {text}
             </h1>
-            <i className={`${iconName} icon`} />
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     );
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
 
 
@@ -45,7 +46,9 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div> Loading!</div>
+        return <div>
+            <Loader />
+        </div>
 
     }
 }
